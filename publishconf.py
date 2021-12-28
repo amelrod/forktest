2e590fc8ae5a27835a742090410a090f35d146fb
Adam Melrod <94829670+amelrod@users.noreply.github.com>
2021-12-28 09:00:28 -0500

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://mathmondays.com"
+SITEURL = "https://amelrod.github.io/forktest/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feed.xml"
